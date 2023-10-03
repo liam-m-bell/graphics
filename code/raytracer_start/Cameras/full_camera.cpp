be3960c1ca59e252050aa33fe289330486920f96
Liam <lmb206@bath.ac.uk>
2023-10-03 21:23:31 +0100

    Line drawer

---
+++ code/raytracer_start/Cameras/full_camera.cpp
@@ -0,0 +1,44 @@
+/****************************************************************************
+*
+* krt - Ken's Raytracer - Coursework Edition. (C) Copyright 1993-2023.
+*
+* I've put a lot of time and effort into this code. For the last decade
+* it's been used to introduce hundreds of students at multiple universities
+* to raytracing. It forms the basis of your coursework but you are free
+* to continue using/developing forever more. However, I ask that you don't
+* share the code or your derivitive versions publicly. In order to continue
+* to be used for coursework and in particular assessment it's important that
+* versions containing solutions are not searchable on the web or easy to
+* download.
+*
+* If you want to show off your programming ability, instead of releasing
+* the code, consider generating an incredible image and explaining how you
+* produced it.
+*/
+
+#include "full_camera.h"
+
+FullCamera::FullCamera()
+{
+
+}
+
+FullCamera::FullCamera(float f, Vertex& p_position, Vector& p_lookat, Vector& p_up)
+{
+
+}
+
+void FullCamera::get_ray_offset(int p_x, int p_y, float p_ox, float p_oy, Ray& p_ray)
+{
+
+}
+
+void FullCamera::get_ray_pixel(int p_x, int p_y, Ray &ray)
+{
+
+}
+
+void FullCamera::render(Environment& env, FrameBuffer& fb)
+{
+
+}
---
+++ code/raytracer_start/Objects/polymesh_object.cpp
@@ -0,0 +1,49 @@
+/****************************************************************************
+*
+* krt - Ken's Raytracer - Coursework Edition. (C) Copyright 1993-2022.
+*
+* I've put a lot of time and effort into this code. For the last decade
+* it's been used to introduce hundreds of students at multiple universities
+* to raytracing. It forms the basis of your coursework but you are free
+* to continue using/developing forever more. However, I ask that you don't
+* share the code or your derivitive versions publicly. In order to continue
+* to be used for coursework and in particular assessment it's important that
+* versions containing solutions are not searchable on the web or easy to
+* download.
+*
+* If you want to show off your programming ability, instead of releasing
+* the code, consider generating an incredible image and explaining how you
+* produced it.
+*/
+
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include "polymesh_object.h"
+
+using namespace std;
+
+
+PolyMesh::PolyMesh(char* file, bool smooth)
+{
+
+    next = 0;
+}
+
+
+
+Hit* PolyMesh::intersection(Ray ray)
+{
+    Hit* hits = 0;
+
+    return hits;
+}
+
+void PolyMesh::apply_transform(Transform& trans)
+{
+
+}
